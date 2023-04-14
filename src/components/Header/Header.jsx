bfc94bbe293385bc97c2c7ddd6a9f6a3c16e21f1
Valentin <valio990210@gmail.com>
2023-04-14 19:59:55 +0300

    link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 
 const Header = () => {
@@ -12,13 +12,15 @@ const Header = () => {
                             <div className="header__top__left">
                                 <span>Need Help?</span>
                                 <span className="header__top__help">
-
+                                <i class="ri-phone-fill"></i> +359 888-103-888
                                 </span>
                             </div>
                         </Col>
 
                         <Col lg='6' md='6' sm='6'>
-
+                            <div className="header__top__right">
+                                <Link to='#'> <i class="ri-login-circle-line"></i>Login</Link>
+                            </div>
                         </Col>
                     </Row>
                 </Container>

d13fd0039c7bdc59b19c2cbcb5542e24f3070e77
Valentin <valio990210@gmail.com>
2023-05-01 11:01:12 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Layout from './components/Layout/Layout'
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <Layout/>
-
-
-
-  )
+  return <Layout />;
 }
 
 export default App;

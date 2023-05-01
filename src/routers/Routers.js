02e01e14331c6e572586815a2567835f8f0f04c0
Valentin <valio990210@gmail.com>
2023-05-01 11:21:38 +0300

    changes

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,25 +1,28 @@
 import React from "react";
-import { Routes, Route, Navigate   } from "react-router-dom";
-import Home from '../pages/Home'
-import About from '../pages/About'
-import CarListing from '../pages/CarListing'
-import CarDetails from '../pages/CarDetails'
-import NotFound from '../pages/NotFound'
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import CarListing from "../pages/CarListing";
+import CarDetails from "../pages/CarDetails";
+import Blog from "../pages/Blog";
+import BlogDetails from "../pages/BlogDetails";
+import NotFound from "../pages/NotFound";
+import Contact from "../pages/Contact";
 
 const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/cars" element={<CarListing />} />
+      <Route path="/cars/:slug" element={<CarDetails />} />
+      <Route path="/blogs" element={<Blog />} />
+      <Route path="/blogs/:slug" element={<BlogDetails />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
-    return (
-        <Routes>
-            <Route path='/' element={Home}> </Route>
-            <Route path='/home' element={Home}> </Route>
-            <Route path='/about' element={About}> </Route>
-            <Route path='/cars' element={CarListing}> </Route>
-            <Route path='/cars/:slug' element={CarDetails}> </Route>
-            <Route path='*' element={NotFound}> </Route>
-
-        </Routes>
-    )
-}
-
-export default Routers
+export default Routers;

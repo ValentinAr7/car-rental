41ef26bee50f3d84871d98e0a4b50a4494ab4550
Valentin <valio990210@gmail.com>
2023-05-01 09:59:00 +0300

    footer css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <Layout/>
+
+
+
   )
 }
 
